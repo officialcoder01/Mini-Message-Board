210ac0864132cede90f5e23bf65bbb197b1cc43d
Zion Musa <musazion986@gmail.com>
2025-10-07 22:09:36 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const indexRouter = require('./routes/indexRouter');
+const path = require('node:path');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use('/', indexRouter);
+
+app.listen(PORT, (error) => {
+    if (error) {
+        throw error;
+    };
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
