d8c1ee3988bfbfc9f6c460e6d15f4bc83fadb3d7
Zion Musa <musazion986@gmail.com>
2025-10-15 19:54:20 +0100

    set view cache to false

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const indexRouter = require('./routes/indexRouter');
@@ -7,6 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
+app.set('view cache', false)
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
